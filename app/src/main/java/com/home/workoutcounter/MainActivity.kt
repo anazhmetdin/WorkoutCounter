4700f9b8de158792e680485de14b25fdbb0f9c8f
anegm98 <a.negm98.an@gmail.com>
2020-05-18 01:45:18 +0200

    add functionality to layout

--- app/src/main/java/com/home/workoutcounter/MainActivity.kt
+++ app/src/main/java/com/home/workoutcounter/MainActivity.kt
@@ -1,20 +1,146 @@
 package com.home.workoutcounter
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.graphics.ImageFormat
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.*
+import android.hardware.camera2.CameraCaptureSession.CaptureCallback
+import android.media.Image
+import android.media.ImageReader
+import android.media.ImageReader.OnImageAvailableListener
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
+import android.os.Environment
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
+import android.util.Size
+import android.view.Surface
+import android.view.TextureView
+import android.view.TextureView.SurfaceTextureListener
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import java.io.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
+    private var takePictureButton: Button? = null
+    private var textureView: TextureView? = null
+    private var cameraId: String? = null
+    protected var cameraDevice: CameraDevice? = null
+    protected var cameraCaptureSessions: CameraCaptureSession? = null
+    private var captureRequestBuilder: CaptureRequest.Builder? = null
+    private var imageDimension: Size? = null
+    private var mBackgroundHandler: Handler? = null
+    private var mBackgroundThread: HandlerThread? = null
+
+    private lateinit var setMinusButton: Button
+    private lateinit var setPlusButton: Button
+    private lateinit var sets: EditText
+
+    private lateinit var repMinusButton: Button
+    private lateinit var repPlusButton: Button
+    private lateinit var reps: EditText
+
+    private lateinit var minutes: TextView
+    private lateinit var seconds: TextView
+
+    private lateinit var repsCountCheckBox: CheckBox
+    private lateinit var repsTimerCheckBox: CheckBox
+    private lateinit var minutesBar: SeekBar
+    private lateinit var secondsBar: SeekBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val setMinusButton: Button = findViewById(R.id.SetMinus)
-        val setPlusButton: Button = findViewById(R.id.SetPlus)
-        val sets: EditText = findViewById(R.id.sets)
+        setMinusButton = findViewById(R.id.SetMinus)
+        setPlusButton = findViewById(R.id.SetPlus)
+        sets = findViewById(R.id.sets)
+
+        repMinusButton = findViewById(R.id.RepMinus)
+        repPlusButton = findViewById(R.id.RepPlus)
+        reps = findViewById(R.id.reps)
+
+        repsCountCheckBox = findViewById(R.id.counterCheckBox)
+        repsTimerCheckBox = findViewById(R.id.timerCheckBox)
+        minutesBar = findViewById(R.id.minutesBar)
+        minutesBar.isEnabled = false
+        secondsBar = findViewById(R.id.secondsBar)
+        secondsBar.isEnabled = false
+        minutes = findViewById(R.id.minutes)
+        seconds = findViewById(R.id.seconds)
+
+        repsCountCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                repMinusButton.isEnabled = true
+                repPlusButton.isEnabled = true
+                reps.isActivated = true
+            } else {
+                repMinusButton.isEnabled = false
+                repPlusButton.isEnabled = false
+                reps.isActivated = false
+            }
+        }
+
+        repsTimerCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                minutesBar.isEnabled = true
+                secondsBar.isEnabled = true
+            } else {
+                minutesBar.isEnabled = false
+                secondsBar.isEnabled = false
+            }
+        }
+
+        minutesBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                // Display the current progress of SeekBar
+                var s = "$i"
+                if (i < 10) {
+                    s = "0$s"
+                }
+                minutes.text = s
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Do something
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Do something
+            }
+        })
+
+        secondsBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                // Display the current progress of SeekBar
+                var s = "$i"
+                if (i < 10) {
+                    s = "0$s"
+                }
+                seconds.text = s
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Do something
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Do something
+            }
+        })
 
         fun addEditable(editableObj: EditText, addition: Int) {
             editableObj.setText((editableObj.text.toString().toInt() + addition).toString())
@@ -25,12 +151,306 @@ class MainActivity : AppCompatActivity() {
         setMinusButton.setOnClickListener {
             if (sets.text.toString().toInt() > 1) {
                 addEditable(sets, -1)
+            } else {
+                addEditable(sets, 998)
             }
         }
         setPlusButton.setOnClickListener {
             if (sets.text.toString().toInt() < 999) {
                 addEditable(sets, 1)
+            } else {
+                addEditable(sets, -998)
             }
         }
+
+        repMinusButton.setOnClickListener {
+            if (reps.text.toString().toInt() > 1) {
+                addEditable(reps, -1)
+            } else {
+                addEditable(reps, 998)
+            }
+        }
+        repPlusButton.setOnClickListener {
+            if (reps.text.toString().toInt() < 999) {
+                addEditable(reps, 1)
+            } else {
+                addEditable(reps, -998)
+            }
+        }
+
+        textureView = findViewById<View>(R.id.texture) as TextureView
+        textureView!!.surfaceTextureListener = textureListener
+        takePictureButton = findViewById<View>(R.id.btn_takepicture) as Button
+        takePictureButton!!.setOnClickListener {
+            takePictureButton!!.isEnabled = false
+            GlobalScope.launch {
+                for (i in 0..10) {
+                    takePicture()
+                    delay(400)
+                }
+                withContext(Dispatchers.Main) {
+                    takePictureButton!!.isEnabled = true
+                    onResume()
+                }
+            }
+        }
+    }
+
+    private var textureListener: SurfaceTextureListener = object : SurfaceTextureListener {
+        override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
+            //open your camera here
+            openCamera()
+        }
+
+        override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture, width: Int, height: Int) {
+            // Transform you image captured size according to the surface width and height
+        }
+
+        override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+            return false
+        }
+
+        override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {}
+    }
+    private val stateCallback: CameraDevice.StateCallback = object : CameraDevice.StateCallback() {
+        override fun onOpened(camera: CameraDevice) {
+            //This is called when the camera is open
+            Log.e(TAG, "onOpened")
+            cameraDevice = camera
+            createCameraPreview()
+        }
+
+        override fun onDisconnected(camera: CameraDevice) {
+            cameraDevice!!.close()
+        }
+
+        override fun onError(camera: CameraDevice, error: Int) {
+            cameraDevice!!.close()
+            cameraDevice = null
+        }
+    }
+
+    private fun startBackgroundThread() {
+        mBackgroundThread = HandlerThread("Camera Background")
+        mBackgroundThread!!.start()
+        mBackgroundHandler = Handler(mBackgroundThread!!.looper)
+    }
+
+    private fun stopBackgroundThread() {
+        mBackgroundThread!!.quitSafely()
+        try {
+            mBackgroundThread!!.join()
+            mBackgroundThread = null
+            mBackgroundHandler = null
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun takePicture() {
+        if (null == cameraDevice) {
+            Log.e(TAG, "cameraDevice is null")
+            return
+        }
+        try {
+            val reader = ImageReader.newInstance(640 / 4, 480 / 4, ImageFormat.YUV_420_888, 1)
+            val outputSurfaces: MutableList<Surface> = ArrayList(2)
+            outputSurfaces.add(reader.surface)
+            outputSurfaces.add(Surface(textureView!!.surfaceTexture))
+            val captureBuilder =
+                cameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE)
+            captureBuilder.addTarget(reader.surface)
+//            val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+//            val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+//            val file = File(storageDir.toString() + "/" + timeStamp + ".yuv")
+            val readerListener: OnImageAvailableListener = object : OnImageAvailableListener {
+                override fun onImageAvailable(reader: ImageReader) {
+                    var image: Image? = null
+                    try {
+                        image = reader.acquireLatestImage()
+                        val buffer = image.planes[0].buffer
+                        val bytes = ByteArray(buffer.capacity())
+                        buffer[bytes]
+                        val bitmapImage = BitmapFactory.decodeByteArray(bytes, 0, bytes.size, null)
+//                        save(bytes)
+                    } catch (e: FileNotFoundException) {
+                        e.printStackTrace()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    } finally {
+                        image?.close()
+                    }
+                }
+
+//                @Throws(IOException::class)
+//                private fun save(bytes: ByteArray) {
+//                    var output: OutputStream? = null
+//                    try {
+//                        output = FileOutputStream(file)
+//                        output.write(bytes)
+//                    } finally {
+//                        output?.close()
+//                    }
+//                }
+            }
+            reader.setOnImageAvailableListener(readerListener, mBackgroundHandler)
+            val captureListener: CaptureCallback = object : CaptureCallback() {
+                override fun onCaptureCompleted(
+                    session: CameraCaptureSession,
+                    request: CaptureRequest,
+                    result: TotalCaptureResult
+                ) {
+                    super.onCaptureCompleted(session, request, result)
+                    createCameraPreview()
+                }
+            }
+            cameraDevice!!.createCaptureSession(
+                outputSurfaces,
+                object : CameraCaptureSession.StateCallback() {
+                    override fun onConfigured(session: CameraCaptureSession) {
+                        try {
+                            session.capture(
+                                captureBuilder.build(),
+                                captureListener,
+                                mBackgroundHandler
+                            )
+                        } catch (e: CameraAccessException) {
+                            e.printStackTrace()
+                        }
+                    }
+
+                    override fun onConfigureFailed(session: CameraCaptureSession) {}
+                },
+                mBackgroundHandler
+            )
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+    }
+
+    protected fun createCameraPreview() {
+        try {
+            val texture = textureView!!.surfaceTexture!!
+            texture.setDefaultBufferSize(640 / 4, 480 / 4)
+            val surface = Surface(texture)
+            captureRequestBuilder =
+                cameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+            captureRequestBuilder!!.addTarget(surface)
+            cameraDevice!!.createCaptureSession(
+                listOf(surface),
+                object : CameraCaptureSession.StateCallback() {
+                    override fun onConfigured(cameraCaptureSession: CameraCaptureSession) {
+                        //The camera is already closed
+                        if (null == cameraDevice) {
+                            return
+                        }
+                        // When the session is ready, we start displaying the preview.
+                        cameraCaptureSessions = cameraCaptureSession
+                        updatePreview()
+                    }
+
+                    override fun onConfigureFailed(cameraCaptureSession: CameraCaptureSession) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Configuration change",
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    }
+                },
+                null
+            )
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun openCamera() {
+        val manager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        Log.e(TAG, "is camera open")
+        try {
+            cameraId = manager.cameraIdList[1]
+            val characteristics = manager.getCameraCharacteristics(cameraId ?: return)
+            val map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)!!
+            imageDimension = map.getOutputSizes(SurfaceTexture::class.java)[0]
+            // Add permission for camera and let user grant the permission
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CAMERA
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    REQUEST_CAMERA_PERMISSION
+                )
+                return
+            }
+            manager.openCamera(cameraId ?: return, stateCallback, null)
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+        Log.e(TAG, "openCamera X")
+    }
+
+    protected fun updatePreview() {
+        if (null == cameraDevice) {
+            Log.e(TAG, "updatePreview error, return")
+        }
+        captureRequestBuilder!!.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
+        try {
+            cameraCaptureSessions!!.setRepeatingRequest(
+                captureRequestBuilder!!.build(),
+                null,
+                mBackgroundHandler
+            )
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                // close the app
+                Toast.makeText(
+                    this@MainActivity,
+                    "Sorry!!!, you can't use this app without granting permission",
+                    Toast.LENGTH_LONG
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e(TAG, "onResume")
+        startBackgroundThread()
+        if (textureView!!.isAvailable) {
+            openCamera()
+        } else {
+            textureView!!.surfaceTextureListener = textureListener
+        }
+    }
+
+    override fun onPause() {
+        Log.e(TAG, "onPause")
+        //closeCamera();
+        stopBackgroundThread()
+        super.onPause()
+    }
+
+    companion object {
+        private const val TAG = "AndroidCameraApi"
+        private const val REQUEST_CAMERA_PERMISSION = 200
     }
 }
